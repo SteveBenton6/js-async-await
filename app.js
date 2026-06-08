38dec7282c127565768ace18480b94f3165815fb
Steve Benton <steve@bentonemail.uk>
2026-06-08 10:51:29 +0100

    add parallel vs sequential example

--- app.js
+++ app.js
@@ -89,5 +89,34 @@ animateRight(btn, 100).catch((err) => {
 // Example 6 -  Parallel Vs. Sequential Requests
 console.log("\nSECTION 6 - Parallel Vs. Sequential Requests");
 
+// SEQUENTIAL REQUESTS
+//  async function get3Pokemon() {
+//    const poke1 = await axios.get("https://pokeapi.co/api/v2/pokemon/1");
+//    const poke2 = await axios.get("https://pokeapi.co/api/v2/pokemon/2");
+//    const poke3 = await axios.get("https://pokeapi.co/api/v2/pokemon/3");
+//    console.log(poke1.data.name);
+//    console.log(poke1.data);
+//    console.log(poke2.data.name);
+//    console.log(poke2.data);
+//    console.log(poke3.data.name);
+//    console.log(poke3.data);
+//  }
+// PARALLEL REQUESTS
+async function get3Pokemon() {
+  const prom1 = axios.get("https://pokeapi.co/api/v2/pokemon/1");
+  const prom2 = axios.get("https://pokeapi.co/api/v2/pokemon/2");
+  const prom3 = axios.get("https://pokeapi.co/api/v2/pokemon/3");
+  const poke1 = await prom1;
+  const poke2 = await prom2;
+  const poke3 = await prom3;
+  console.log(poke1.data.name);
+  console.log(poke1.data);
+  console.log(poke2.data.name);
+  console.log(poke2.data);
+  console.log(poke3.data.name);
+  console.log(poke3.data);
+}
+get3Pokemon();
+
 // Example 7 -  Refactoring with Promise.all
 console.log("\nSECTION 7 - Refactoring with Promise.all");
